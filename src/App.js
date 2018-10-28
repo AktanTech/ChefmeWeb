be1149539b11052f07c8d19e82c2f06eec94f5c0
GunnerStraiker1 <victorcito001@hotmail.com>
2018-10-28 08:54:45 -0600

    Home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import {Button} from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <div id="main"></div>
+        <Button><img src="desayuno.png"></img></Button>
       </div>
     );
   }
